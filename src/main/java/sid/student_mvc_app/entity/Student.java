45d0d94beb9517dc7a7aebd7b4eb1a59b8b30e42
Reda-23 <readhajjaj@gmail.com>
2022-11-23 19:35:39 +0100

    S&G

--- src/main/java/sid/student_mvc_app/entity/Student.java
+++ src/main/java/sid/student_mvc_app/entity/Student.java
@@ -16,4 +16,38 @@ public class Student {
     private String lastname;
     @Column(name = "EMAIL")
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
