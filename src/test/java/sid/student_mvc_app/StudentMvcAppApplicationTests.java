ce99aa772078665fcfde2b7af940709d80d4e923
Reda-23 <readhajjaj@gmail.com>
2022-11-23 19:23:20 +0100

    Creating the project

---
+++ src/test/java/sid/student_mvc_app/StudentMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sid.student_mvc_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMvcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
