438cfe1eb07d2958e34532bc6e055efb72ff3057
Reda-23 <readhajjaj@gmail.com>
2022-11-23 19:54:05 +0100

    Creating The Service Interface

---
+++ src/main/java/sid/student_mvc_app/service/StudentService.java
@@ -0,0 +1,9 @@
+package sid.student_mvc_app.service;
+
+import sid.student_mvc_app.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+}
