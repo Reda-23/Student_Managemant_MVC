62fe2277e1edf7fda059deb6ff5931f2929d9354
Reda-23 <readhajjaj@gmail.com>
2022-11-25 17:23:09 +0100

    Adding Data To The App

--- src/main/java/sid/student_mvc_app/StudentMvcAppApplication.java
+++ src/main/java/sid/student_mvc_app/StudentMvcAppApplication.java
@@ -1,13 +1,28 @@
 package sid.student_mvc_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sid.student_mvc_app.entity.Student;
+import sid.student_mvc_app.repository.StudentRepository;
 
 @SpringBootApplication
 public class StudentMvcAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentMvcAppApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner start(StudentRepository studentRepository){
+        return args -> {
+                studentRepository.save(new Student(null,"Reda","Hajjaj","readhajjaj@gmail.com"));
+                studentRepository.save(new Student(null,"Driss","Ihratten","drissKhanez7.com"));
+                studentRepository.save(new Student(null,"Ali","Chkhiro","AliCh88@gmail.com"));
+                studentRepository.save(new Student(null,"Youssef","BenElAbbas","youssefben30@gmail.com"));
+        };
     }
 
 }
